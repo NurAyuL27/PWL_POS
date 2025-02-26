b2c7a832e75229918e45e0b51a46047be951b595
Nuraayu <nurayu2718@gmail.com>
2025-02-26 22:01:56 +0700

    praktikum 2

---
+++ database/migrations/2025_02_26_110533_create_m_barang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->string('barang_kode', 10)->unique();
+            $table->unsignedBigInteger('kategori_id');
+            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori')->onDelete('cascade');
+            $table->string('nama_barang', 100);
+            $table->integer('stok');
+            $table->decimal('harga_beli', 10, 2);
+            $table->decimal('harga_jual', 10, 2);
+            $table->timestamps();
+        });        
+    }
+    
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};

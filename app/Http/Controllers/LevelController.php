336f242944aaef7515c11eecfc863adfafd2003a
Nuraayu <nurayu2718@gmail.com>
2025-03-19 09:24:30 +0700

    pt6_Tugas_m_supplier

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -149,7 +149,7 @@ class LevelController extends Controller
             'level_kode' => 'required|string|max:10'
         ]);
 
-        levelModel::find($id)->update([
+        LevelModel::find($id)->update([
             'level_nama' => $request->level_nama,
             'level_kode' => $request->level_kode,
         ]);

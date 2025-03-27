d28c9072da4ea375132d9191749b47b7360f77e1
Nuraayu <nurayu2718@gmail.com>
2025-03-27 22:29:40 +0700

    pt7_Tugas3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -8,19 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeUser
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next, ... $roles): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user_role = $request->user()->getRole();  // ambil data level_kode dari user yg login
+        $user = $request->user(); 
 
-        if (in_array($user_role, $roles)) { // cek apakah level_kode user ada di dalam array roles
-            return $next($request); // jika ada, maka lanjutkan request
+        if (!$user || !in_array($user->getRole(), $roles)) {
+            abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
         }
-        // jika tidak punya role, maka tampilkan error 403
-        abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
+
+        return $next($request);
     }
 }

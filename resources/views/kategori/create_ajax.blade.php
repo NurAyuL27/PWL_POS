75feae2a58fd11c6dd83c06070951a31ac232a74
Nuraayu <nurayu2718@gmail.com>
2025-04-21 21:08:40 +0700

    update_create

--- resources/views/kategori/create_ajax.blade.php
+++ resources/views/kategori/create_ajax.blade.php
@@ -1,35 +1,37 @@
 <form action="{{ url('/kategori/ajax') }}" method="POST" id="form-tambah">
     @csrf
-    <div class="modal-header">
-        <h5 class="modal-title">Tambah Data kategori</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <div class="modal-body">
-        <div class="form-group">
-            <label>Nama kategori</label>
-            <input type="text" name="kategori_nama" id="kategori_nama" class="form-control" required>
-            <small id="error-nama" class="error-text form-text text-danger"></small>
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Kategori</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>Kategori Kode</label>
+                    <input value="" type="text" name="kategori_kode" id="kategori_kode" class="form-control" required>
+                    <small id="error-kategori-kode" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Kategori Nama</label>
+                    <input value="" type="text" name="kategori_nama" id="kategori_nama" class="form-control" required>
+                    <small id="error-kategori-nama" class="error-text form-text text-danger"></small>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
         </div>
-        <div class="form-group">
-            <label>Kode kategori</label>
-            <input type="text" name="kategori_kode" id="kategori_kode" class="form-control" required>
-            <small id="error-kode" class="error-text form-text text-danger"></small>
-        </div>
-    </div>
-    <div class="modal-footer">
-        <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
-        <button type="submit" class="btn btn-primary">Simpan</button>
     </div>
 </form>
-
 <script>
     $(document).ready(function () {
         $("#form-tambah").validate({
             rules: {
-                nama: { required: true, minlength: 3, maxlength: 100 },
-                kategori_kode: { required: true, maxlength: 5 }
+                kategori_kode: { required: true, minlength: 3, maxlength: 20 },
+                kategori_nama: { required: true, minlength: 3, maxlength: 100 }
             },
             submitHandler: function (form) {
                 $.ajax({
@@ -38,7 +40,7 @@
                     data: $(form).serialize(),
                     success: function (response) {
                         if (response.status) {
-                            $('#modal-crud').modal('hide');
+                            $('#myModal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
                                 title: 'Berhasil',
@@ -48,7 +50,7 @@
                         } else {
                             $('.error-text').text('');
                             $.each(response.msgField, function (prefix, val) {
-                                $('#error-' + prefix).text(val[0]);
+                                $('#error-'+prefix).text(val[0]);
                             });
                             Swal.fire({
                                 icon: 'error',
@@ -65,10 +67,10 @@
                 error.addClass('invalid-feedback');
                 element.closest('.form-group').append(error);
             },
-            highlight: function (element) {
+            highlight: function (element, errorClass, validClass) {
                 $(element).addClass('is-invalid');
             },
-            unhighlight: function (element) {
+            unhighlight: function (element, errorClass, validClass) {
                 $(element).removeClass('is-invalid');
             }
         });
--- resources/views/level/create_ajax.blade.php
+++ resources/views/level/create_ajax.blade.php
@@ -1,38 +1,37 @@
 <form action="{{ url('/level/ajax') }}" method="POST" id="form-tambah">
     @csrf
-    <div class="modal-header">
-        <h5 class="modal-title">Tambah Data Level</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <div class="modal-body">
-        <div class="form-group">
-            <label>Nama Level</label>
-            <input type="text" name="level_nama" id="level_nama" class="form-control" required>
-            <small id="error-nama" class="error-text form-text text-danger"></small>
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Level</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>Level Kode</label>
+                    <input value="" type="text" name="level_kode" id="level_kode" class="form-control" required>
+                    <small id="error-level-kode" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Level Nama</label>
+                    <input value="" type="text" name="level_nama" id="level_nama" class="form-control" required>
+                    <small id="error-level-nama" class="error-text form-text text-danger"></small>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
         </div>
-        <div class="form-group">
-            <label>Kode Level</label>
-            <input type="text" name="level_kode" id="level_kode" class="form-control" required>
-            <small id="error-kode" class="error-text form-text text-danger"></small>
-            @error('level_kode')
-                <small class="form-text text-danger">{{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-    <div class="modal-footer">
-        <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
-        <button type="submit" class="btn btn-primary">Simpan</button>
     </div>
 </form>
-
 <script>
     $(document).ready(function () {
         $("#form-tambah").validate({
             rules: {
-                nama: { required: true, minlength: 3, maxlength: 100 },
-                level_kode: { required: true, maxlength: 5 }
+                level_kode: { required: true, minlength: 3, maxlength: 20 },
+                level_nama: { required: true, minlength: 3, maxlength: 100 }
             },
             submitHandler: function (form) {
                 $.ajax({
@@ -41,7 +40,7 @@
                     data: $(form).serialize(),
                     success: function (response) {
                         if (response.status) {
-                            $('#modal-crud').modal('hide');
+                            $('#myModal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
                                 title: 'Berhasil',
@@ -51,7 +50,7 @@
                         } else {
                             $('.error-text').text('');
                             $.each(response.msgField, function (prefix, val) {
-                                $('#error-' + prefix).text(val[0]);
+                                $('#error-'+prefix).text(val[0]);
                             });
                             Swal.fire({
                                 icon: 'error',
@@ -68,10 +67,10 @@
                 error.addClass('invalid-feedback');
                 element.closest('.form-group').append(error);
             },
-            highlight: function (element) {
+            highlight: function (element, errorClass, validClass) {
                 $(element).addClass('is-invalid');
             },
-            unhighlight: function (element) {
+            unhighlight: function (element, errorClass, validClass) {
                 $(element).removeClass('is-invalid');
             }
         });

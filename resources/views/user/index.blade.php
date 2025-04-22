4df0de3001ceb6558939c276d49ec526b8fd5524
Nuraayu <nurayu2718@gmail.com>
2025-04-22 19:24:21 +0700

    update_m_user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,51 +3,48 @@
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">Daftar Pengguna</h3>
+        <h3 class="card-title">Daftar User</h3>
         <div class="card-tools">
-            <button onclick="modalAction('{{ url('user/import') }}')" class="btn btn-info">Import Pengguna</button>
-            <a href="{{ url('/user/export_excel') }}" class="btn btn-primary"><i class="fa fa-fileexcel"></i> Export User Excel</a>
-            <a href="{{ url('/user/export_pdf') }}" class="btn btn-warning"><i class="fa fa-filepdf"></i> Export User PDF</a>
-            <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
+            <button onclick="modalAction('{{ url('/user/import') }}')" class="btn btn-info">Import User</button>
+            <a href="{{ url('/user/export_excel') }}" class="btn btn-primary"><i class="fa fa-fileexcel"></i>Export User (Excel)</a>
+            <a href="{{ url('/user/export_pdf') }}" class="btn btn-warning"><i class="fa fa-filepdf"></i> Export User (PDF)</a>
+            <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-success">Tambah Data Ajax</button>
         </div>
     </div>
-
     <div class="card-body">
-        <!-- untuk Filter data -->
-        <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group form-group-sm row text-sm mb-0">
-                        <label for="filter_level" class="col-md-1 col-form-label">Filter</label>
-                        <div class="col-md-3">
-                            <select name="filter_level" class="form-control form-control-sm filter_level">
-                                <option value="">- Semua -</option>
-                                @foreach($level as $l)
-                                    <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Level Pengguna</small>
-                        </div>
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+        @if (session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+
+        <div class="row mb-3">
+            <div class="col-md-6">
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Filter:</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" id="level_id" name="level_id">
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
                     </div>
                 </div>
             </div>
         </div>
 
-        @if(session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
-        @if(session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
-        @endif
-
-        <table class="table table-bordered table-sm table-striped table-hover" id="table-user">
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
-                    <th>No</th>
-                    <th>Username</th>
-                    <th>Nama</th>
-                    <th>Level Pengguna</th>
-                    <th>Aksi</th>
+                    <th style="text-align: center;">No</th>
+                    <th style="text-align: center;">ID Pengguna</th>
+                    <th style="text-align: center;">Username</th>
+                    <th style="text-align: center;">Nama</th>
+                    <th style="text-align: center;">Level Pengguna</th>
+                    <th style="text-align: center;">Aksi</th>
                 </tr>
             </thead>
             <tbody></tbody>
@@ -55,7 +52,7 @@
     </div>
 </div>
 
-<div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false" data-width="75%"></div>
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('js')
@@ -66,34 +63,38 @@
         });
     }
 
-    var tableUser;
+    var dataUser;
     $(document).ready(function () {
-        tableUser = $('#table-user').DataTable({
+        dataUser = $('#table_user').DataTable({
             processing: true,
-            serverSide: true,
+            serverSide: true, // Menggunakan server-side processing
             ajax: {
-                "url": "{{ url('user/list') }}",
-                "dataType": "json",
-                "type": "POST",
-                "data": function (d) {
-                    d.filter_level = $('.filter_level').val();
+                url: "{{ url('user/list') }}",
+                type: "POST",
+                dataType: "json",
+                data: function (d) {
+                    d.level_id = $('#level_id').val();
                 }
             },
             columns: [
                 {
-                    data: null,
+                    data: "DT_RowIndex",
                     className: "text-center",
-                    width: "5%",
+                    width: "4%",
                     orderable: false,
-                    searchable: false,
-                    render: function (data, type, row, meta) {
-                        return meta.row + 1;
-                    }
+                    searchable: false
+                },
+                {
+                    data: "user_id",
+                    className: "text-center",
+                    width: "10%",
+                    orderable: true,
+                    searchable: false
                 },
                 {
                     data: "username",
                     className: "",
-                    width: "25%",
+                    width: "20%",
                     orderable: true,
                     searchable: true
                 },
@@ -109,26 +110,19 @@
                     className: "",
                     width: "20%",
                     orderable: true,
-                    searchable: false
+                    searchable: true
                 },
                 {
                     data: "aksi",
                     className: "text-center",
-                    width: "20%",
                     orderable: false,
                     searchable: false
                 }
             ]
         });
 
-        $('#table-user_filter input').unbind().bind().on('keyup', function (e) {
-            if (e.keyCode == 13) {
-                tableUser.search(this.value).draw();
-            }
-        });
-
-        $('.filter_level').change(function () {
-            tableUser.draw();
+        $('#level_id').on('change', function () {
+            dataUser.ajax.reload();
         });
     });
 </script>

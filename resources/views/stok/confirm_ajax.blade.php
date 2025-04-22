547dcf587046f7b584f1423dc1c33154073836b9
Nuraayu <nurayu2718@gmail.com>
2025-04-22 22:53:44 +0700

    update_t_stok

--- resources/views/stok/confirm_ajax.blade.php
+++ resources/views/stok/confirm_ajax.blade.php
@@ -1,15 +1,13 @@
 @empty($stok)
-    <div id="modal-delete" class="modal-dialog modal-lg" role="document">
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <div class="alert alert-danger">
-                    <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
+                    <h5><i class="fas fa-ban"></i> Kesalahan!!!</h5>
                     Data yang anda cari tidak ditemukan
                 </div>
                 <a href="{{ url('/stok') }}" class="btn btn-warning">Kembali</a>
@@ -17,64 +15,76 @@
         </div>
     </div>
 @else
-    <form action="{{ url('/stok/' . $stok->stok_id . '/delete_ajax') }}" method="POST" id="form-delete">
+    <form action="{{ url('/stok/' . $stok->stok_id.'/delete_ajax') }}" method="POST" id="form-delete">
         @csrf
         @method('DELETE')
-        <div class="modal-header">
-            <h5 class="modal-title">Hapus Data Stok</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <div class="alert alert-warning">
-                <h5><i class="icon fas fa-ban"></i> Konfirmasi !!!</h5>
-                Apakah Anda ingin menghapus data seperti di bawah ini?
+        <div id="modal-master" class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data Stok</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="alert alert-warning">
+                        <h5><i class="fas fa-ban"></i> Konfirmasi !!!</h5>
+                        Apakah Anda ingin menghapus data berikut?
+                    </div>
+                    <table class="table-sm table-bordered table-striped w-100">
+                        <tr>
+                            <th class="text-right col-3">Nama Barang:</th>
+                            <td class="col-9">{{ $stok->barang->nama_barang }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-right">Nama Penyetok:</th>
+                            <td>{{ $stok->user->nama }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-right">Tanggal Stok:</th>
+                            <td>{{ \Carbon\Carbon::parse($stok->stok_tanggal)->translatedFormat('d F Y') }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-right">Jumlah Stok:</th>
+                            <td>{{ number_format($stok->stok_jumlah, 0, ',', '.') }}</td>
+                        </tr>
+                    </table>
+                </div>
+                
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-warning" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Ya, Hapus</button>
+                </div>
             </div>
-            <table class="table table-sm table-bordered table-striped">
-                <tr>
-                    <th class="text-right col-3">Nama Barang:</th>
-                    <td class="col-9">{{ $stok->barang->nama_barang }}</td>
-                </tr>
-                <tr>
-                    <th class="text-right">Nama Penyetok:</th>
-                    <td>{{ $stok->user->nama }}</td>
-                </tr>
-                <tr>
-                    <th class="text-right">Tanggal Stok:</th>
-                    <td>{{ \Carbon\Carbon::parse($stok->stok_tanggal)->translatedFormat('d F Y') }}</td>
-                </tr>
-                <tr>
-                    <th class="text-right">Jumlah Stok:</th>
-                    <td>{{ number_format($stok->stok_jumlah, 0, ',', '.') }}</td>
-                </tr>
-            </table>
-        </div>
-        <div class="modal-footer">
-            <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
-            <button type="submit" class="btn btn-primary">Ya, Hapus</button>
         </div>
     </form>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             $("#form-delete").validate({
                 rules: {},
-                submitHandler: function (form) {
+                submitHandler: function(form) {
                     $.ajax({
                         url: form.action,
                         type: form.method,
                         data: $(form).serialize(),
-                        success: function (response) {
-                            if (response.status) {
-                                $('#modal-crud').modal('hide');
-                                Swal.fire({ icon: 'success', title: 'Berhasil', text: response.message });
+                        success: function(response) {
+                            if(response.status){
+                                $('#myModal').modal('hide');
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Berhasil',
+                                    text: response.message
+                                });
                                 dataStok.ajax.reload();
                             } else {
                                 $('.error-text').text('');
-                                $.each(response.msgField, function (prefix, val) {
-                                    $('#error-' + prefix).text(val[0]);
+                                $.each(response.msgField, function(prefix, val) {
+                                    $('#error-'+prefix).text(val[0]);
                                 });
-                                Swal.fire({ icon: 'error', title: 'Terjadi Kesalahan', text: response.message });
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Terjadi Kesalahan',
+                                    text: response.message
+                                });
                             }
                         }
                     });
--- resources/views/stok/show_ajax.blade.php
+++ resources/views/stok/show_ajax.blade.php
@@ -1,8 +1,8 @@
 @empty($stok)
-    <div id="modal-delete" class="modal-dialog modal-lg" role="document">
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
+                <h5 class="modal-title">Kesalahan</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -10,45 +10,48 @@
             <div class="modal-body">
                 <div class="alert alert-danger">
                     <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
-                    Data yang anda cari tidak ditemukan
+                    Data yang anda cari tidak ditemukan.
                 </div>
                 <a href="{{ url('/stok') }}" class="btn btn-warning">Kembali</a>
             </div>
         </div>
     </div>
 @else
-    <div class="modal-header">
-        <h5 class="modal-title">Data Stok Barang</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <div class="modal-body">
-        <table class="table table-sm table-bordered table-striped">
-            <tr>
-                <th class="text-right col-3">ID Stok :</th>
-                <td class="col-9">{{ $stok->stok_id }}</td>
-            </tr>
-            <tr>
-                <th class="text-right col-3">Nama Barang :</th>
-                <td class="col-9">{{ $stok->barang->nama_barang }}</td>
-            </tr>
-            <tr>
-                <th class="text-right col-3">Nama Penyetok :</th>
-                <td class="col-9">{{ $stok->user->nama }}</td>
-            </tr>
-            <tr>
-                <th class="text-right col-3">Tanggal Stok :</th>
-                <td class="col-9">{{ \Carbon\Carbon::parse($stok->stok_tanggal)->format('Y-m-d') }}</td>
-            </tr>
-            <tr>
-                <th class="text-right col-3">Jumlah Stok :</th>
-                <td class="col-9">{{ $stok->stok_jumlah }}</td>
-            </tr>
-        </table>
-    </div>
-    <div class="modal-footer">
-        <button onclick="modalAction('{{ url('/stok/' . $stok->stok_id . '/edit_ajax') }}')" class="btn btn-success btn-sm">Edit</button>
-        <button type="button" data-dismiss="modal" class="btn btn-primary btn-sm">Tutup</button>
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Detail Stok</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-bordered">
+                    <tr>
+                        <th width="25%">ID Stok</th>
+                        <td>{{ $stok->stok_id }}</td>
+                    </tr>
+                    <tr>
+                        <th>Nama Barang</th>
+                        <td>{{ $stok->barang->nama_barang}}</td>
+                    </tr>
+                    <tr>
+                        <th>Nama Penyetok</th>
+                        <td>{{ $stok->user->nama}}</td>
+                    </tr>
+                    <tr>
+                        <th>Tanggal Stok</th>
+                        <td>{{ \Carbon\Carbon::parse($stok->stok_tanggal)->format('Y-m-d') }}</td>
+                    </tr>
+                    <tr>
+                        <th>Jumlah Stok</th>
+                        <td>{{ $stok->stok_jumlah }}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-primary">Tutup</button>
+            </div>
+        </div>
     </div>
 @endempty

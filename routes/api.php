9973ecc08e77041f66f41ecbf4639b41fa3a1ae1
Nuraayu <nurayu2718@gmail.com>
2025-05-01 21:31:54 +0700

    pt10_prak3

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', RegisterController::class);
+Route::post('/register', RegisterController::class)->name('register');
+
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login'); 
+Route::middleware('auth:api')->get('/user', function (Request $request) { 
+    return $request->user();
+});
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');

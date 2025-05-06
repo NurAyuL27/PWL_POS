f454a5c3860378b1939d0d88dfaea8ccd7c8ee14
Nuraayu <nurayu2718@gmail.com>
2025-05-06 19:40:20 +0700

    pt11_Tugas_m_barang

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\BarangModel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -12,8 +13,50 @@ class BarangController extends Controller
     }
 
     public function store(Request $request) {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        //$barang = BarangModel::create($request->all());
+        //return response()->json($barang, 201);
+
+        // set validation 
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required|exists:m_kategori,kategori_id',
+            'barang_kode' => 'required|unique:m_barang',
+            'nama_barang' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        // if validations fails 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // save image file to storage
+        $image = $request->file('image');
+        $image->storeAs('public/barang', $image->hashName());
+
+        // create barang 
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'nama_barang' => $request->nama_barang,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName(),
+        ]);
+
+        // return response JSON barang is created 
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+
+        // return JSON process insert failed 
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang) {
--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
@@ -12,10 +13,21 @@ class BarangModel extends Model
 
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
-    protected $fillable = ['barang_kode', 'nama_barang', 'kategori_id', 'stok', 'harga_beli', 'harga_jual'];
+    //protected $fillable = ['barang_kode', 'nama_barang', 'kategori_id', 'stok', 'harga_beli', 'harga_jual'];
+    protected $fillable = [
+    'kategori_id', 'barang_kode', 'nama_barang', 'harga_beli', 'harga_jual', 'image', 'created_at', 'updated_at'
+    ];
+
 
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/barang/' . $image),
+        );
     }
 }
